3a9572c78c3596adbcf3e04868e0a630b9026007
Nathan Ahrens <nathanmahrens@gmail.com>
2019-04-26 14:10:16 -0400

    links.txt should now be strictly json, not just line be line

--- youtube-mp3.py
+++ youtube-mp3.py
@@ -1,15 +1,27 @@
-from __future__ import unicode_literals
-import youtube_dl
+from __future__ import unicode_literals 
+import youtube_dl 
+import json 
+import os 
 
-with open('links.txt', 'r') as file:
-    ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-    'key': 'FFmpegExtractAudio',
-    'preferredcodec': 'mp3',
-    'preferredquality': '192',
-    }],
-    }
-    for line in file:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([line])
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
+
+try:
+    with open(os.path.join(BASE_DIR,'links.txt'), 'r') as file:
+        songs = json.load(file)
+        for song in  songs:
+            ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': song['title'] + '.%(ext)s',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'postprocessor_args': ['-metadata', 'artist='+song['artist'], '-metadata', 'title=' + song['title'], '-metadata', 'album=' + song['album']],
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([song['url']])
+    os.remove(os.path.join(BASE_DIR, 'links.txt'))
+except FileNotFoundError:
+    print("file doesn't exist...")
+
