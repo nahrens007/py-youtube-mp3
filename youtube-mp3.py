0416ac118f00b29d755e37e4777916bc386b57eb
Nathan Ahrens <redninja2@users.noreply.github.com>
2020-04-04 07:33:00 -0400

    Added logging, modified json handling.

--- youtube-mp3.py
+++ youtube-mp3.py
@@ -2,14 +2,22 @@ from __future__ import unicode_literals
 import youtube_dl
 import json
 import os
+from datetime import datetime
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+if not os.path.isdir(os.path.join(BASE_DIR,'logs')):
+    # create logs/ dir
+    os.mkdir(os.path.join(BASE_DIR,'logs'))
+    
 with open(os.path.join(BASE_DIR,'links.txt'), 'r') as file:
-    for line in file:
-        if line.startswith("#"):
-            continue
-        song = json.loads(line)
+    songs = json.loads(file.read())
+    now = datetime.now()
+    current_time = now.strftime("%Y%m%d%H%M%S")
+    log=open(os.path.join(BASE_DIR,'logs','yt_dl_'+current_time+'.log'),'w')
+    log.write(json.dumps(songs,sort_keys=True, indent=4, separators=(',',': ')))
+    log.close()
+    for song in songs:
         ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': song['title'] + '.%(ext)s',
@@ -25,6 +33,6 @@ with open(os.path.join(BASE_DIR,'links.txt'), 'r') as file:
 
 
 f = open(os.path.join(BASE_DIR,'links.txt'), 'w')
-#f.write('#{"url":"YoutubeLink","artist":"SongArtist","title":"SongTitle","album":"SongAlbum"}') # empty the file
+#file format: [{"url":"YoutubeLink","artist":"SongArtist","title":"SongTitle","album":"SongAlbum"}]
 f.write('')
 f.close()
